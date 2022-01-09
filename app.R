17e94e8b5083b3a03725992327a9cd6ae9b38ff5
Anthony <anthony.yoskovich@gmail.com>
2022-01-09 09:46:32 -0500

    Removing old stack overflow file selector

--- app.R
+++ app.R
@@ -1,15 +1,20 @@
 library(shiny)
 
+ALL_FILES <- list.files('www')
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Image Viewer"),
     sidebarLayout(
         position = 'right',
         sidebarPanel(
-            uiOutput("select.folder"),
-            uiOutput('select.file'),
+            selectInput(
+                "file_options", 
+                label='Select a file',
+                choices = ALL_FILES
+            )
         ),
         mainPanel(
             code(textOutput('text')),
@@ -19,35 +24,24 @@ ui <- fluidPage(
             img(src='car_smiling.jpg', height=500, width=500),
             dateRangeInput('dates', h3('Date Range')),
             br(), br(),
-            textOutput('selected_date')
+            textOutput('selected_date'),
+            textOutput('selected_file')
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    root <- "C:/Users/anthony/Desktop/img_viewer"
-    
-    output$text <- renderText({input$file.name})
-    
-    output$select.folder <- renderUI(expr = selectInput(
-        inputId = 'folder.name',
-        label = 'Folder Name',
-        choices = list.dirs(path = root, full.names = FALSE, recursive = FALSE)
-    ))
-    
-    output$select.file <- renderUI(expr = selectInput(
-            inputId = 'file.name',
-            label = 'File Name',
-            choices = list.files(path = file.path(root, input$folder.name))
-    ))
     
     output$selected_date <- renderText({
         paste(
             'Start Date: ', input$dates[[1]],
             'End Date: ', input$dates[[2]]
         )
+    })
+    
+    output$selected_file <- renderText({
+        paste('You selected ', input$file_options)
     })
 }
 
