f9fe4b7370ed0168747bb96f85b572f7901d359a
Anthony <anthony.yoskovich@gmail.com>
2022-01-09 09:42:22 -0500

    Adding online tutorial examples

--- app.R
+++ app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
@@ -14,28 +5,21 @@ ui <- fluidPage(
 
     # Application title
     titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
     sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot"),
-           img(src='myImage.jpg', align = "right")
-        )
-    ),
-    
-    fluidRow(
+        position = 'right',
         sidebarPanel(
             uiOutput("select.folder"),
-            uiOutput('select.file')
+            uiOutput('select.file'),
+        ),
+        mainPanel(
+            code(textOutput('text')),
+            br(), br(), br(),
+            
+            # App will search www/ for images
+            img(src='car_smiling.jpg', height=500, width=500),
+            dateRangeInput('dates', h3('Date Range')),
+            br(), br(),
+            textOutput('selected_date')
         )
     )
 )
@@ -43,28 +27,28 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-    
     root <- "C:/Users/anthony/Desktop/img_viewer"
-    output$select.folder <-
-        renderUI(expr = selectInput(inputId = 'folder.name',
-                                    label = 'Folder Name',
-                                    choices = list.dirs(path = root,
-                                                        full.names = FALSE,
-                                                        recursive = FALSE)))
     
-    output$select.file <-
-        renderUI(expr = selectInput(inputId = 'file.name',
-                                    label = 'File Name',
-                                    choices = list.files(path = file.path(root,
-                                                                          input$folder.name))))
+    output$text <- renderText({input$file.name})
+    
+    output$select.folder <- renderUI(expr = selectInput(
+        inputId = 'folder.name',
+        label = 'Folder Name',
+        choices = list.dirs(path = root, full.names = FALSE, recursive = FALSE)
+    ))
+    
+    output$select.file <- renderUI(expr = selectInput(
+            inputId = 'file.name',
+            label = 'File Name',
+            choices = list.files(path = file.path(root, input$folder.name))
+    ))
+    
+    output$selected_date <- renderText({
+        paste(
+            'Start Date: ', input$dates[[1]],
+            'End Date: ', input$dates[[2]]
+        )
+    })
 }
 
 # Run the application 
