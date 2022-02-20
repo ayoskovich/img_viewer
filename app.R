e72720456e256c33988ef4061affc81bea268400
Anthony <anthony.yoskovich@gmail.com>
2022-02-19 22:15:46 -0500

    Displaying multiple images using loop

--- app.R
+++ app.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(lubridate)
 
 ALL_FILES <- list.files('www', pattern='*.jpg|*.png')
+LOG_FILE <- 'logfile.rds'
 
 ui <- fluidPage(
   #tags$head(
@@ -16,21 +17,36 @@ ui <- fluidPage(
       titlePanel('Image Tagger'),
       mainPanel(
         selectInput('which_img', label='Image File', choices=ALL_FILES),
-        selectInput('iso_tag', label='ISO', choices=c(100, 200, 400)),
+        radioButtons('iso_tag', label='ISO', choices=c(100, 200, 400)),
         
         # Hacky way to remove whitespace around resized image
-        HTML("<div style='height: 300px;'>"),
+        HTML("<div style='height: 250px;'>"),
         imageOutput('show_image'),
         HTML("</div>"),
         
         actionButton('send', 'Update output file'),
         dataTableOutput('logdata')
       )
+    ),
+    tabPanel('Search',
+      titlePanel('Image Search'),
+      mainPanel(
+        radioButtons('which_iso', label='ISO', choices=c(100, 200, 400)),
+        uiOutput('myboxes')
+      )
     )
   )
 )
   
 server <- function(input, output) {
+    
+    IMG_LIST <- list('A.jpg', 'B.jpg')
+    v <- list()
+    for (i in 1:length(IMG_LIST)){
+      v[[i]] <- img(src=IMG_LIST[i], height='150px')
+    }
+    
+    output$myboxes <- renderUI(v)
     
     output$show_image <- renderImage({
         list(
@@ -39,17 +55,22 @@ server <- function(input, output) {
         )
     })
     
-    output$logdata <- renderDataTable({
-      read_rds('logfile.rds') %>% 
-        filter(img == input$which_img) %>% 
-        filter(dt == max(dt)) %>% 
-        mutate(across(everything(), as.character)) %>% 
-        pivot_longer(
-          everything(),
-          names_to='Variable',
-          values_to='Value'
-        )
-    },
+    output$logdata <- renderDataTable(
+      {
+        read_rds(LOG_FILE) %>% 
+          filter(img == input$which_img) %>% 
+          filter(dt == max(dt)) %>% 
+          select(
+            `Last Updated` = dt,
+            `ISO` = iso
+          ) %>% 
+          mutate(across(everything(), as.character)) %>% 
+          pivot_longer(
+            everything(),
+            names_to='Variable',
+            values_to='Value'
+          )
+      },
       options=list(
         info=F,
         searching=F,
@@ -60,17 +81,17 @@ server <- function(input, output) {
     )
     
     observeEvent(input$send, {
-      read_rds('logfile.rds') %>% 
+      read_rds(LOG_FILE) %>% 
         bind_rows(
           tribble(
             ~dt, ~img, ~iso, 
             now(), input$which_img, as.integer(input$iso_tag)
           )
         ) %>% 
-        write_rds('logfile.rds')
+        write_rds(LOG_FILE)
       
       showNotification(
-        'Data updated!', 
+        'Data updated', 
         duration = 3, 
         closeButton = T,
         type='message'
