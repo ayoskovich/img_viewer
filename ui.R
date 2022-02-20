1cd4d46ff818cb338c6c8e8e0e1ebd043f21b515
Anthony <anthony.yoskovich@gmail.com>
2022-02-20 17:17:06 -0500

    Using shinydashboard

--- ui.R
+++ ui.R
@@ -1,37 +1,43 @@
 
-shinyUI(fluidPage(
-  tabsetPanel(
-    tabPanel(
-      "Tagger",
-      titlePanel("Image Tagger"),
-      mainPanel(
-        selectInput(
-          "filterSet",
-          label = "Which files to view?",
-          choices = c("All files", "Untagged")
+shinyUI(dashboardPage(
+  dashboardHeader(title='Image Tagger'),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem('Edit Tags', tabName = 'tagger', icon = icon('dashboard')),
+      menuItem('Photo Search', tabName = 'viewer', icon = icon('dashboard'))
+    )
+  ),
+    dashboardBody(
+      tabItems(
+        tabItem(
+          tabName = 'tagger',
+          h1('Modify Tags'),
+            selectInput(
+              "filterSet",
+              label = "Which files to view?",
+              choices = c("All files", "Untagged")
+            ),
+            selectInput(
+              "which_img",
+              label = "Image File",
+              choices = ALL_FILES
+            ),
+            radioButtons("iso_tag", label = "ISO", choices = ISO_CHOICES),
+            
+            # Hacky way to remove whitespace around resized image
+            HTML("<div style='height: 250px;'>"),
+            imageOutput("show_image"),
+            HTML("</div>"),
+            actionButton("send", "Update output file"),
+            dataTableOutput("logdata")
         ),
-        selectInput(
-          "which_img",
-          label = "Image File",
-          choices = ALL_FILES
-        ),
-        radioButtons("iso_tag", label = "ISO", choices = ISO_CHOICES),
-
-        # Hacky way to remove whitespace around resized image
-        HTML("<div style='height: 250px;'>"),
-        imageOutput("show_image"),
-        HTML("</div>"),
-        actionButton("send", "Update output file"),
-        dataTableOutput("logdata")
-      )
-    ),
-    tabPanel(
-      "Search",
-      titlePanel("Image Search"),
-      mainPanel(
-        radioButtons("which_iso", label = "ISO", choices = ISO_CHOICES),
-        uiOutput("myboxes")
+        tabItem(
+          tabName = 'viewer',
+          h1('Search for images'),
+          radioButtons("which_iso", label = "ISO", choices = ISO_CHOICES),
+          uiOutput("myboxes")
+        )
       )
     )
   )
-))
+)
