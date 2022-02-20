89a4a145c028924f8d83740ee9890031722c52c6
Anthony <anthony.yoskovich@gmail.com>
2022-02-20 17:17:52 -0500

    Styling

--- ui.R
+++ ui.R
@@ -1,43 +1,42 @@
 
 shinyUI(dashboardPage(
-  dashboardHeader(title='Image Tagger'),
+  dashboardHeader(title = "Image Tagger"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem('Edit Tags', tabName = 'tagger', icon = icon('dashboard')),
-      menuItem('Photo Search', tabName = 'viewer', icon = icon('dashboard'))
+      menuItem("Edit Tags", tabName = "tagger", icon = icon("dashboard")),
+      menuItem("Photo Search", tabName = "viewer", icon = icon("dashboard"))
     )
   ),
-    dashboardBody(
-      tabItems(
-        tabItem(
-          tabName = 'tagger',
-          h1('Modify Tags'),
-            selectInput(
-              "filterSet",
-              label = "Which files to view?",
-              choices = c("All files", "Untagged")
-            ),
-            selectInput(
-              "which_img",
-              label = "Image File",
-              choices = ALL_FILES
-            ),
-            radioButtons("iso_tag", label = "ISO", choices = ISO_CHOICES),
-            
-            # Hacky way to remove whitespace around resized image
-            HTML("<div style='height: 250px;'>"),
-            imageOutput("show_image"),
-            HTML("</div>"),
-            actionButton("send", "Update output file"),
-            dataTableOutput("logdata")
+  dashboardBody(
+    tabItems(
+      tabItem(
+        tabName = "tagger",
+        h1("Modify Tags"),
+        selectInput(
+          "filterSet",
+          label = "Which files to view?",
+          choices = c("All files", "Untagged")
         ),
-        tabItem(
-          tabName = 'viewer',
-          h1('Search for images'),
-          radioButtons("which_iso", label = "ISO", choices = ISO_CHOICES),
-          uiOutput("myboxes")
-        )
+        selectInput(
+          "which_img",
+          label = "Image File",
+          choices = ALL_FILES
+        ),
+        radioButtons("iso_tag", label = "ISO", choices = ISO_CHOICES),
+
+        # Hacky way to remove whitespace around resized image
+        HTML("<div style='height: 250px;'>"),
+        imageOutput("show_image"),
+        HTML("</div>"),
+        actionButton("send", "Update output file"),
+        dataTableOutput("logdata")
+      ),
+      tabItem(
+        tabName = "viewer",
+        h1("Search for images"),
+        radioButtons("which_iso", label = "ISO", choices = ISO_CHOICES),
+        uiOutput("myboxes")
       )
     )
   )
-)
+))
